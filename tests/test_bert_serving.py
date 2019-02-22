a3ca3ec02ed317a094281200782955c08d453d3b
hanhxiao <hanhxiao@tencent.com>
2019-02-22 14:38:47 +0800

    add bert test

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -5,7 +5,7 @@ from bert_serving.client import BertClient
 
 class TestBertServing(unittest.TestCase):
     def test_bert_client(self):
-        bc = BertClient()
+        bc = BertClient(timeout=2000)
         vec = bc.encode(['你好么？我很好', '你好么？我很好!但 我还没吃'])
         self.assertEqual(vec.shape[0], 2)
         self.assertEqual(vec.shape[1], 768)
