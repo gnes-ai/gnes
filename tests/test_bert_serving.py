f1205e12e78e1c687a2f5f8b3ddbb4748c4877c9
hanhxiao <hanhxiao@tencent.com>
2019-02-22 19:02:54 +0800

    change to a new ip

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -1,10 +1,27 @@
 import os
+import time
 import unittest
 
 from bert_serving.client import BertClient
+from bert_serving.server import BertServer
+from bert_serving.server.helper import get_args_parser
 
 
 class TestBertServing(unittest.TestCase):
+    @staticmethod
+    def get_test_parser():
+        parser = get_args_parser()
+        parser.set_defaults(model_dir=os.environ['BERT_CI_MODEL'],
+                            port=int(os.environ['BERT_CI_PORT']),
+                            port_out=int(os.environ['BERT_CI_PORT_OUT']))
+        return parser
+
+    def setUp(self):
+        args = self.get_test_parser().parse_args()
+        self.server = BertServer(args)
+        self.server.start()
+        time.sleep(30)
+
     def test_bert_client(self):
         bc = BertClient(timeout=2000, port=int(os.environ['BERT_CI_PORT']),
                         port_out=int(os.environ['BERT_CI_PORT_OUT']))
