e23e75752f9c7ca5779b3af14037eb95286c2d0f
hanhxiao <hanhxiao@tencent.com>
2019-02-22 15:14:42 +0800

    add bert test

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -4,6 +4,13 @@ from bert_serving.client import BertClient
 
 
 class TestBertServing(unittest.TestCase):
+    def test_tf(self):
+        import tensorflow as tf
+        print(tf.__version__)
+        a = tf.constant(1, dtype=tf.int32)
+        with tf.Session() as sess:
+            self.assertEqual(sess.run(a), 1)
+
     def test_bert_client(self):
         bc = BertClient(timeout=2000)
         vec = bc.encode(['你好么？我很好', '你好么？我很好!但 我还没吃'])
