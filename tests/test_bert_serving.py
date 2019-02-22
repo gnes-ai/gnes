107b8d142ccf8fd5726a4cebff2e41256c9d5579
hanhxiao <hanhxiao@tencent.com>
2019-02-22 17:53:10 +0800

    change to a new ip

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -4,14 +4,7 @@ from bert_serving.client import BertClient
 
 
 class TestBertServing(unittest.TestCase):
-    def test_tf(self):
-        import tensorflow as tf
-        print(tf.__version__)
-        a = tf.constant(1, dtype=tf.int32)
-        with tf.Session() as sess:
-            self.assertEqual(sess.run(a), 1)
-
-    def _test_bert_client(self):
+    def test_bert_client(self):
         bc = BertClient(timeout=2000)
         vec = bc.encode(['你好么？我很好', '你好么？我很好!但 我还没吃'])
         self.assertEqual(vec.shape[0], 2)
