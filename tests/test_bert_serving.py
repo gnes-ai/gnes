bf673549ce160676b063ec9ce4eadd6dbb350cf3
hanhxiao <hanhxiao@tencent.com>
2019-02-22 18:19:49 +0800

    change to a new ip

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from bert_serving.client import BertClient
@@ -5,7 +6,8 @@ from bert_serving.client import BertClient
 
 class TestBertServing(unittest.TestCase):
     def test_bert_client(self):
-        bc = BertClient(timeout=2000)
+        bc = BertClient(timeout=2000, port=int(os.environ['BERT_CI_PORT']),
+                        port_out=int(os.environ['BERT_CI_PORT_OUT']))
         vec = bc.encode(['你好么？我很好', '你好么？我很好!但 我还没吃'])
         self.assertEqual(vec.shape[0], 2)
         self.assertEqual(vec.shape[1], 768)
