3be17617f98bd6780d66ff0178f8e9a18eb26cba
hanhxiao <hanhxiao@tencent.com>
2019-02-27 11:21:02 +0800

    add more test for encode

--- tests/test_lopq.py
+++ tests/test_lopq.py
@@ -33,12 +33,25 @@ class TestPCA(unittest.TestCase):
         self.assertEqual(self.test_vecs.shape[0] * int(self.k / self.m),
                          len(out))
 
-    def test_encode_single(self):
+    def test_encode_gpu_false(self):
         lopq = LOPQEncoder(self.k, self.m, self.num_clusters)
         lopq.train(self.test_vecs)
-        out = lopq.encode_cpu(self.test_vecs[:10])
+        out = lopq.encode(self.test_vecs[:10], use_gpu=True)
         self.assertEqual(10 * int(self.k / self.m), len(out))
         self.assertEqual(bytes, type(out))
+        out2 = lopq.encode(self.test_vecs[:10], use_gpu=False)
+        self.assertEqual(out, out2)
+
+    def test_encode_batching(self):
+        lopq = LOPQEncoder(self.k, self.m, self.num_clusters)
+        lopq.train(self.test_vecs)
+        out = lopq.encode(self.test_vecs, use_gpu=True, batch_size=32)
+        self.assertEqual(self.test_vecs.shape[0] * int(self.k / self.m), len(out))
+        self.assertEqual(bytes, type(out))
+        out2 = lopq.encode(self.test_vecs, use_gpu=True, batch_size=64)
+        self.assertEqual(out, out2)
+        out3 = lopq.encode(self.test_vecs, use_gpu=False, batch_size=64)
+        self.assertEqual(out, out3)
 
 
 if __name__ == '__main__':
