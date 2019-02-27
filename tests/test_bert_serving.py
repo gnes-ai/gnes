a0f58455c3429c46aed8059153d037d8547bdd7e
hanhxiao <hanhxiao@tencent.com>
2019-02-27 18:29:20 +0800

    fix path issue

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -2,7 +2,6 @@ import os
 import time
 import unittest
 
-from bert_serving.client import BertClient
 from bert_serving.server import BertServer
 from bert_serving.server.helper import get_args_parser
 
@@ -28,13 +27,6 @@ class TestBertServing(unittest.TestCase):
         time.sleep(30)
 
     def test_bert_client(self):
-        bc = BertClient(timeout=2000, port=int(os.environ['BERT_CI_PORT']),
-                        port_out=int(os.environ['BERT_CI_PORT_OUT']))
-        vec = bc.encode(['你好么？我很好', '你好么？我很好!但 我还没吃'])
-        self.assertEqual(vec.shape[0], 2)
-        self.assertEqual(vec.shape[1], 768)
-        bc.close()
-
         bbe = BertBinaryEncoder(port=int(os.environ['BERT_CI_PORT']),
                                 port_out=int(os.environ['BERT_CI_PORT_OUT']),
                                 timeout=10000)  # set timeout larger as we do actual encoding
