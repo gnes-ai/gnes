72cc81aee447bd9db3ab591382de77a77140d8ae
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-02-28 13:02:33 +0800

    add leveldb

--- src/nes/indexer/__init__.py
+++ src/nes/indexer/__init__.py
@@ -1,7 +1,8 @@
 from typing import List, Tuple, Any
-
+import json
 from ..document import BaseDocument
 from ..helper import set_logger
+from .leveldb import BaseLVDB
 
 
 class BaseIndexer:
@@ -29,8 +30,30 @@ class BaseBinaryIndexer(BaseIndexer):
 
 
 class BaseTextIndexer(BaseIndexer):
-    def add(self, docs: List[BaseDocument]):
-        pass
+    def __init__(self, data_path: str):
+        super().__init__()
+        self.basedb = BaseLVDB(data_path)
 
-    def query(self, keys: List[int]) -> List[BaseDocument]:
-        pass
+    def add(self, docs: List[BaseDocument]):
+        doc_ids = [bytes(str(doc._id), 'utf8') for doc in docs]
+        contents = [self.doc2bytes(doc) for doc in docs]
+        self.basedb.add(doc_ids, contents)
+
+    def query(self, keys: List[int]) -> List[dict]:
+        doc_ids = [bytes(str(_id), 'utf8') for _id in keys]
+        contents = self.basedb.query(doc_ids)
+        return [self.bytes2doc(content) for content in contents]
+
+    def doc2bytes(self, doc: BaseDocument):
+        # doc._sentences is iterator, which can't be dumped.
+        return bytes(json.dumps({
+                            'id': doc._id,
+                            'sentences': list(doc._sentences),
+                            'content': doc._content},
+                           ensure_ascii=False), 'utf8')
+
+    def bytes2doc(self, content):
+        if content:
+            return json.loads(content.decode())
+        else:
+            return {}
