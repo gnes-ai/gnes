b3eb18adb42fc3090776eb37b0900c601df57760
hanhxiao <hanhxiao@tencent.com>
2019-02-28 15:19:54 +0800

    refactor bert binary

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -37,7 +37,8 @@ class TestBertServing(unittest.TestCase):
         self.assertEqual(vec.shape[0], len(self.test_data))
         self.assertEqual(vec.shape[1], 768)
 
-        bbe = BertBinaryEncoder(port=int(os.environ['BERT_CI_PORT']),
+        bbe = BertBinaryEncoder(pca_output_dim=32,
+                                port=int(os.environ['BERT_CI_PORT']),
                                 port_out=int(os.environ['BERT_CI_PORT_OUT']))
         self.assertRaises(RuntimeError, bbe.encode)
 
