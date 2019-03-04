d2b650652f956627cf9997c9a65c790c6324a6c0
hanhxiao <hanhxiao@tencent.com>
2019-03-04 15:53:26 +0800

    put nes to bert serving test unit

--- tests/test_bert_serving.py
+++ tests/test_bert_serving.py
@@ -6,6 +6,8 @@ from bert_serving.client import BertClient
 from bert_serving.server import BertServer
 from bert_serving.server.helper import get_args_parser
 
+from src.nes import DummyNES
+from src.nes.document import UniSentDocument, MultiSentDocument
 from src.nes.encoder.bert_binary import BertBinaryEncoder
 
 
@@ -14,9 +16,13 @@ class TestBertServing(unittest.TestCase):
     def setUp(self):
         dirname = os.path.dirname(__file__)
         self.dump_path = os.path.join(dirname, 'encoder.bin')
+        self.db_path = './test_leveldb'
 
         with open(os.path.join(dirname, 'tangshi.txt'), encoding='utf8') as fp:
             self.test_data = [v.strip() for v in fp if v.strip() and len(v.strip()) > 10]
+            tmp = self.test_data
+            self.test_data1 = [UniSentDocument(t) for t in tmp]
+            self.test_data2 = [MultiSentDocument(t) for t in tmp]
 
         print('load %d lines of sentences' % len(self.test_data))
 
@@ -55,6 +61,18 @@ class TestBertServing(unittest.TestCase):
         bbe2 = bbe.load(self.dump_path)
         out2 = bbe2.encode(self.test_data)
         self.assertEqual(out, out2)
+
+        nes = DummyNES(pca_output_dim=32,
+                       cluster_per_byte=8,
+                       port=int(os.environ['BERT_CI_PORT']),
+                       port_out=int(os.environ['BERT_CI_PORT_OUT']),
+                       data_path=self.db_path)
+        self.assertRaises(RuntimeError, nes.add, self.test_data1)
+        self.assertRaises(RuntimeError, nes.query, self.test_data1, 1)
+        nes.train(self.test_data1)
+        nes.add(self.test_data1)
+        query = [s for d in self.test_data1 for s in d.sentences]
+        print(nes.query(query, top_k=1))
 
     def tearDown(self):
         self.server.close()
