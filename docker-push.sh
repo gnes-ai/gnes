3350ab7eaebfb1f9273a9895e7f367358fe374f1
hanhxiao <hanhxiao@tencent.com>
2019-03-06 18:26:43 +0800

    add docker service

--- docker-push.sh
+++ docker-push.sh
@@ -3,6 +3,7 @@
 set -e
 
 GIT_TAG=$1
+PORT=$2
 if [ -z "$1" ]
   then
     GIT_TAG=$(git rev-parse --short HEAD)
@@ -12,8 +13,8 @@ PROJ_NAME=aipd-nes
 
 printf "your current git commit tag: \e[1;33m$GIT_TAG\e[0m\n"
 
-docker build --rm -t docker.oa.com/public/$PROJ_NAME:$GIT_TAG .
-docker push docker.oa.com/public/$PROJ_NAME:$GIT_TAG
+docker build --rm -t docker.oa.com$PORT/public/$PROJ_NAME:$GIT_TAG .
+docker push docker.oa.com$PORT/public/$PROJ_NAME:$GIT_TAG
 
 echo 'done! and to run the container simply do:'
 printf "\e[1;33mdocker run --network=host -v /data1/cips/data:/ext_data -it docker.oa.com/public/$PROJ_NAME:$GIT_TAG bash\e[0m\n"
