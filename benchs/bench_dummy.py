a4699cb9f6e73ba7f352f7b2b38e1bfe67ca7b4a
larryjianfeng <geozheng@100.102.33.163>
2019-03-06 10:36:44 +0800

    bug fix

--- benchs/bench_dummy.py
+++ benchs/bench_dummy.py
@@ -5,7 +5,8 @@ sys.path.append('../')
 from src.nes import DummyNES
 from src.nes.document import UniSentDocument
 import time
-
+os.environ['BERT_CI_PORT'] = '7125'
+os.environ['BERT_CI_PORT_OUT'] = '7126'
 
 def get_docs():
     with open('./test/doc.txt', 'r') as f:
@@ -32,8 +33,9 @@ doc = get_docs()
 queries, labels = get_query()
 db_path = './test_leveldb'
 
-nes = DummyNES(pca_output_dim=32,
-               cluster_per_byte=8,
+nes = DummyNES(pca_output_dim=200,
+			   num_bytes=20,
+               cluster_per_byte=255,
                port=int(os.environ['BERT_CI_PORT']),
                port_out=int(os.environ['BERT_CI_PORT_OUT']),
                data_path=db_path)
