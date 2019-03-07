825d283f01d675c5668ea2b2d76fb11aa192b19b
hanhxiao <hanhxiao@tencent.com>
2019-03-07 10:33:49 +0800

    change to idc

--- docker-push.sh
+++ docker-push.sh
@@ -4,6 +4,8 @@ set -e
 
 GIT_TAG=$1
 PORT=$2
+PROXY=$3
+
 if [ -z "$1" ]
   then
     GIT_TAG=$(git rev-parse --short HEAD)
@@ -13,7 +15,7 @@ PROJ_NAME=aipd-nes
 
 printf "your current git commit tag: \e[1;33m$GIT_TAG\e[0m\n"
 
-docker build --rm -t docker.oa.com$PORT/public/$PROJ_NAME:$GIT_TAG .
+docker build $PROXY --rm -t docker.oa.com$PORT/public/$PROJ_NAME:$GIT_TAG .
 docker push docker.oa.com$PORT/public/$PROJ_NAME:$GIT_TAG
 
 echo 'done! and to run the container simply do:'
