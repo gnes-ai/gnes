b8cc97bc2692f37e9922fee4a8bfa4e2e76a49d5
hanhxiao <hanhxiao@tencent.com>
2019-03-15 15:25:01 +0800

    fix unit test

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -85,13 +85,6 @@ class TestYaml(unittest.TestCase):
                                              cluster_per_byte=8,
                                              port=1,
                                              port_out=2, ignore_all_checks=True)
-        self._test_different_encoder_yamlize(DummyNES, num_bytes=8,
-                                             pca_output_dim=32,
-                                             cluster_per_byte=8,
-                                             port=1,
-                                             port_out=2,
-                                             data_path=self.db_path,
-                                             ignore_all_checks=True)
 
     def _test_different_encoder_yamlize(self, cls, *args, **kwargs):
         a = cls(*args, **kwargs)
@@ -102,3 +95,12 @@ class TestYaml(unittest.TestCase):
         self.assertEqual(type(b), cls)
         self.assertEqual(a._init_kwargs_dict, b._init_kwargs_dict)
         b.close()
+
+    def test_NES_yaml_dump(self):
+        self._test_different_encoder_yamlize(DummyNES, num_bytes=8,
+                                             pca_output_dim=32,
+                                             cluster_per_byte=8,
+                                             port=1,
+                                             port_out=2,
+                                             data_path=self.db_path,
+                                             ignore_all_checks=True)
