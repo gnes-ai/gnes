9d67d2b0750359dd3a68c6137d115d2a12d055ed
hanhxiao <hanhxiao@tencent.com>
2019-04-03 19:39:11 +0800

    fix indexer cli

--- docker-push.sh
+++ docker-push.sh
@@ -9,19 +9,19 @@ _docker_push() {
 
     if [ -z "$2" ]
       then
-        printf "you are publishing a new version: \e[1;33m$GIT_TAG\e[0m\n"
-        docker build --target $TARGET --build-arg HTTP_PROXY=${HTTP_PROXY} --build-arg HTTPS_PROXY=${HTTPS_PROXY} --build-arg NO_PROXY=${NO_PROXY} --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} --network=host --rm -t docker.oa.com:8080/public/$PROJ_NAME:$GIT_TAG .
-        docker push docker.oa.com:8080/public/$PROJ_NAME:$GIT_TAG
-      else
+        GIT_TAG=$(git rev-parse --short HEAD)
         printf "your current git commit tag: \e[1;33m$GIT_TAG\e[0m\n"
         docker build --rm --target $TARGET -t docker.oa.com/public/${PROJ_NAME}:$GIT_TAG .
         docker push docker.oa.com/public/$PROJ_NAME:$GIT_TAG
         printf 'done! and to run the container simply do:\n'
         printf "\e[1;33mdocker run --rm -v /data1/cips/data:/ext_data -it docker.oa.com/public/$PROJ_NAME:$GIT_TAG bash\e[0m\n"
+      else
+        printf "you are publishing a new version: \e[1;33m$GIT_TAG\e[0m\n"
+        docker build --target $TARGET --build-arg HTTP_PROXY=${HTTP_PROXY} --build-arg HTTPS_PROXY=${HTTPS_PROXY} --build-arg NO_PROXY=${NO_PROXY} --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} --network=host --rm -t docker.oa.com:8080/public/$PROJ_NAME:$GIT_TAG .
+        docker push docker.oa.com:8080/public/$PROJ_NAME:$GIT_TAG
     fi
 }
 
-GIT_VER=$(git rev-parse --short HEAD)
-_docker_push "base" $GIT_VER
-_docker_push "encoder" $GIT_VER
-_docker_push "indexer" $GIT_VER
+_docker_push "base"
+_docker_push "encoder"
+_docker_push "indexer"
