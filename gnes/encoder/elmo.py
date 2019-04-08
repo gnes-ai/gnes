94a66c1cd98a3f438c401d7ea9bbe9bd9684be45
felix <madwang@tencent.com>
2019-04-08 13:22:58 +0800

    refactor elmo

--- gnes/encoder/elmo.py
+++ gnes/encoder/elmo.py
@@ -6,7 +6,7 @@ import numpy as np
 from ..helper import batching, cn_tokenizer
 
 from .base import BaseEncoder, CompositionalEncoder
-from .models.hit_elmo import HitElmo
+from elmoformanylangs import Embedder
 
 
 class ElmoEncoder(BaseEncoder):
@@ -15,7 +15,27 @@ class ElmoEncoder(BaseEncoder):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.elmo_encoder = HitElmo(*args, **kwargs)
+        model_dir = kwargs.get('model_dir', None)
+        if model_dir is None:
+            raise ValueError('model_dir argument is not specified!')
+        self.model_dir = model_dir
+
+        config_path = kwargs.get('config_path', None)
+        if config_path is None:
+            raise ValueError('config_path is not specified!')
+
+        with open(config_path, 'r') as f:
+            self.config = json.load(f)
+
+        self.batch_size = kwargs.get('batch_size', 64)
+        self.pooling_layer = kwargs.get('pooling_layer', -1)
+        if self.pooling_layer > 2:
+            raise ValueError('pooling_layer = %d is not supported now!'
+                                     % self.pooling_layer)
+        self.pooling_strategy = kwargs.get('pooling_strategy', None)
+
+        self._elmo = Embedder(model_dir=self.model_dir, batch_size=self.batch_size)
+
         self.is_trained = True
         self._encoder_args = args
         self._encoder_kwargs = kwargs
@@ -26,17 +46,47 @@ class ElmoEncoder(BaseEncoder):
         batch_tokens = []
         for sent in text:
             batch_tokens.append(cn_tokenizer.tokenize(sent))
-        return self.elmo_encoder.predict(batch_tokens, *args, **kwargs)
+
+        elmo_encodes = self._elmo.sents2elmo(batch_tokens, output_layer=-2)
+
+        pooled_data = []
+        for token_encodes in elmo_encodes:
+            _layer_data = None
+            if self.pooling_layer == -1:
+                _layer_data = np.average(token_encodes, axis=0)
+            elif self.pooling_layer >= 0:
+                _layer_data = token_encodes[self.pooling_layer]
+            else:
+                raise ValueError('pooling_layer = %d is not supported now!'
+                                     % self.pooling_layer)
+
+            _pooled_data = None
+            if self.pooling_strategy is None or self.pooling_strategy == 'NONE':
+                _pooled_data = payload
+            elif self.pooling_strategy == 'REDUCE_MEAN':
+                _pooled_data = np.mean(payload, axis=0)
+            elif self.pooling_strategy == 'REDUCE_MAX':
+                _pooled_data = np.amax(payload, axis=0)
+            elif self.pooling_strategy == 'REDUCE_MEAN_MAX':
+                _pooled_data = np.concatenate(
+                    (np.mean(payload, axis=0), np.amax(payload, axis=0)),
+                    axis=1)
+            else:
+                raise ValueError(
+                    'pooling_strategy: %s has not been implemented' %
+                    self.pooling_strategy)
+            pooled_data.append(_pooled_data)
+        return pooled_data
+
 
     def __getstate__(self):
         d = super().__getstate__()
-        del d['elmo_encoder']
+        del d['_elmo']
         return d
 
     def __setstate__(self, d):
         super().__setstate__(d)
-        # TODO: ...
-        self.elmo_encoder = NULL
+        self._elmo = Embedder(model_dir=self.model_dir, batch_size=self.batch_size)
 
     def close(self):
-        self.elmo_encoder.close()
+        super().close()
--- gnes/encoder/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_torch import BaseTorchModel
-from .hit_elmo import HitElmo
-
-__all__ = ['BaseTorchModel', 'HitElmo']
