ce3de4b391dc082c9606202b8e774227dbdcb6b0
felix <madwang@tencent.com>
2019-04-10 21:53:26 +0800

    add openai gpt pretrained model (only english)

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'gputil>=1.4.0',
         'elmoformanylangs @ git+https://github.com/HIT-SCIR/ELMoForManyLangs.git@master#egg=elmoformanylangs-0.0.2',
         'flair>=0.4.1'
+        'pytorch-pretrained-bert>=0.6.1',
     ],
     extras_require={
         'test': ['pylint'],
