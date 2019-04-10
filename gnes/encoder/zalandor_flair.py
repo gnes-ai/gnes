c9bed2c5559d01cf01309f59f84351cd43d5135d
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-04-10 16:41:20 +0800

    add flair encoding

---
+++ gnes/encoder/zalandor_flair.py
@@ -0,0 +1,61 @@
+from typing import List
+import numpy as np
+from .base import BaseEncoder
+from ..helper import batching
+from flair.data import Sentence
+from flair.embeddings import FlairEmbeddings
+
+
+flair_embedding_forward = FlairEmbeddings('multi-forward-fast')
+
+
+class FlairEncoder(BaseEncoder):
+
+    def __init__(self, model_name: str = 'multi-forward-fast',
+                 batch_size: int = 64,
+                 pooling_strategy: str = 'REDUCE_MEAN', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.model_name = model_name
+
+        self.batch_size = batch_size
+        self.pooling_strategy = pooling_strategy
+
+        self._flair = FlairEmbeddings(self.model_name)
+        self.is_trained = True
+
+    @batching
+    def encode(self, text: List[str], *args, **kwargs) -> np.ndarray:
+        # tokenize text
+        batch_tokens = [Sentence(sent) for sent in text]
+
+        flair_encodes = self._flair.embed(batch_tokens)
+
+        pooled_data = []
+        for sentence in flair_encodes:
+            _layer_data = [s.embedding for s in sentence]
+            if self.pooling_strategy is None or self.pooling_strategy == 'NONE':
+                _pooled_data = _layer_data
+            elif self.pooling_strategy == 'REDUCE_MEAN':
+                _pooled_data = np.mean(_layer_data, axis=0)
+            elif self.pooling_strategy == 'REDUCE_MAX':
+                _pooled_data = np.amax(_layer_data, axis=0)
+            elif self.pooling_strategy == 'REDUCE_MEAN_MAX':
+                _pooled_data = np.concatenate(
+                    (np.mean(_layer_data, axis=0), np.amax(_layer_data, axis=0)),
+                    axis=1)
+            else:
+                raise ValueError(
+                    'pooling_strategy: %s has not been implemented' %
+                    self.pooling_strategy)
+            pooled_data.append(_pooled_data)
+        return np.asarray(pooled_data, dtype=np.float32)
+
+    def __getstate__(self):
+        d = super().__getstate__()
+        del d['_flair']
+        return d
+
+    def __setstate__(self, d):
+        super().__setstate__(d)
+        self._flair = FlairEmbeddings(self.model_name)
