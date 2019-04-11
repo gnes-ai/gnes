ac93d26991ad76203410c924c4e5a134143998ba
felix <madwang@tencent.com>
2019-04-11 11:34:31 +0800

    fix error in setup

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'memory_profiler>=0.55.0',
         'gputil>=1.4.0',
         'elmoformanylangs @ git+https://github.com/HIT-SCIR/ELMoForManyLangs.git@master#egg=elmoformanylangs-0.0.2',
-        'flair>=0.4.1'
+        'flair>=0.4.1',
         'pytorch-pretrained-bert>=0.6.1',
     ],
     extras_require={
