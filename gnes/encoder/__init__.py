ce9c401bbb870ed574a12d50ceaf10d143e7ba52
felix <madwang@tencent.com>
2019-04-11 18:51:12 +0800

    fix ...

--- gnes/encoder/__init__.py
+++ gnes/encoder/__init__.py
@@ -3,7 +3,6 @@ from .bert import BertEncoder, BertEncoderServer
 from .elmo import ElmoEncoder
 from .gpt import GPTEncoder
 from .gpt2 import GPT2Encoder
-from .transformer_xl import TransformerXLEncoder
 from .pca import PCALocalEncoder
 from .pq import PQEncoder
 from .tf_pq import TFPQEncoder
@@ -18,5 +17,4 @@ __all__ = ['BaseEncoder',
            'PipelineEncoder',
            'ElmoEncoder',
            'GPTEncoder',
-           'GPT2Encoder',
-           'TransformerXLEncoder']
+           'GPT2Encoder',]
