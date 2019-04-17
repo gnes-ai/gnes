fc64a04492e5932ef6f21e86dc3f6e1f6cde90cf
hanhxiao <hanhxiao@tencent.com>
2019-04-17 20:01:18 +0800

    add dialog api

--- shell/example-1.sh
+++ shell/example-1.sh
@@ -5,30 +5,27 @@ source "./dialog.sh"
 
 aa=$(TITLE='select your docker image source from the list:';
      TITLE_SHORT='example of ui.show_options';
-     OPTIONS=(0 "docker.oa"
-              1 "Tencent Cloud"
-              2 "official Dockerhub")
-     DEFAULT_OPTION=0;
+     OPTIONS=("docker.oa"
+              "Tencent Cloud"
+              "official Dockerhub")
+     DEFAULT_VALUE=0;
      ui.show_options)
 
 bb=$(TITLE='use random port?';
      TITLE_SHORT='example of ui.show_yesno';
+     DEFAULT_VALUE='n';
      ui.show_yesno)
 
 cc=$(TITLE='what is the name of your server';
      TITLE_SHORT='example of ui.show_input';
+     DEFAULT_VALUE='test me!';
      ui.show_input)
 
-dd=$(TITLE='this is just a message to inform user, information collected';
+dd=$(TITLE='this is just a message to inform user\nnote that this block the process';
      TITLE_SHORT='example of ui.show_msgbox';
+     BLOCK_INPUT=0;
      ui.show_msgbox)
 
 ee=$(TITLE="1. you choose $aa\n2. you choose $bb\n3. you type $cc\n";
      TITLE_SHORT='example of ui.show_msgbox';
      ui.show_msgbox)
-
-
-printf "1. you choose $aa\n"
-printf "2. you choose $bb\n"
-printf "3. you type $cc\n"
-printf "4. you choose $dd\n"
