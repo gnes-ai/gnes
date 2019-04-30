0d8c5c6683c356191bb8da65bd7a8386ea0d976c
hanhxiao <hanhxiao@tencent.com>
2019-04-30 13:16:42 +0800

    fix indexer

--- gnes/indexer/bindexer/__init__.py
+++ gnes/indexer/bindexer/__init__.py
@@ -47,9 +47,8 @@ class BIndexer(BaseBinaryIndexer):
         self.bindexer = IndexCore(self.num_bytes, 4, self.ef,
                                   self.insert_iterations,
                                   self.query_iterations)
-
-        if self.bindexer.load(self.indexer_bin_path) < 0:
-            self.logger.warning('fail to load model from %s, will init an empty one' % self.indexer_bin_path)
+        if os.path.exists(self.indexer_bin_path):
+            self.bindexer.load(self.indexer_bin_path)
 
     def add(self, doc_ids: List[int], vectors: bytes, *args, **kwargs):
         if len(vectors) != len(doc_ids) * self.num_bytes:
