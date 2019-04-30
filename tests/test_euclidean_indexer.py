f36f9f09e799757a1f542741e051bdb81b5dcfde
hanhxiao <hanhxiao@tencent.com>
2019-04-30 12:55:49 +0800

    fix indexer

--- tests/test_euclidean_indexer.py
+++ tests/test_euclidean_indexer.py
@@ -15,14 +15,15 @@ class TestEUIndexer(unittest.TestCase):
         self.add_label = np.random.randint(0, 1e9, [1000]).tolist()
 
         self.sub_query = self.toy_query[:10]
-        self.dump_path = './test_eu_indexer'
+        self.data_path = './test_eu_indexer'
+        self.dump_path = './test_eu_indexer/indexer.bin'
 
     def tearDown(self):
-        if os.path.exists(self.dump_path):
-            shutil.rmtree(self.dump_path)
+        if os.path.exists(self.data_path):
+            shutil.rmtree(self.data_path)
 
     def test_add(self):
-        fd = FaissIndexer(20, 'HNSW32', self.dump_path)
+        fd = FaissIndexer(20, 'HNSW32', self.data_path)
         fd.add(self.toy_label, self.toy_query)
         self.assertEqual(fd.size, self.toy_query.shape[0])
         fd.add(self.add_label, self.add_query)
@@ -30,14 +31,14 @@ class TestEUIndexer(unittest.TestCase):
                          self.toy_query.shape[0] + self.add_query.shape[0])
 
     def test_query(self):
-        fd = FaissIndexer(20, 'HNSW32', self.dump_path)
+        fd = FaissIndexer(20, 'HNSW32', self.data_path)
         fd.add(self.toy_label, self.toy_query)
         ret = fd.query(self.sub_query, top_k=5)
         self.assertEqual(len(ret), self.sub_query.shape[0])
         self.assertEqual(len(ret[0]), 5)
 
     def test_dump_load(self):
-        with FaissIndexer(20, 'HNSW32', self.dump_path) as tmp:
+        with FaissIndexer(20, 'HNSW32', self.data_path) as tmp:
             tmp.add(self.toy_label, self.toy_query)
             tmp.dump(self.dump_path)
 
