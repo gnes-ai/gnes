08ba87d2b0b82954e04008c69815f96d8ec5cd02
hanhxiao <hanhxiao@tencent.com>
2019-04-30 13:23:48 +0800

    fix indexer

--- tests/test_bindexer.py
+++ tests/test_bindexer.py
@@ -1,11 +1,11 @@
 import os
-import unittest
 import shutil
+import unittest
 
 import numpy as np
 
-from gnes.indexer.bindexer import BIndexer
 from gnes.helper import touch_dir
+from gnes.indexer.bindexer import BIndexer
 
 
 class TestFIndexer(unittest.TestCase):
@@ -39,7 +39,7 @@ class TestFIndexer(unittest.TestCase):
         fd.add(self.toy_label, self.toy_data.tobytes())
         rs = fd.query(self.toy_query.tobytes(), 2, method='nsw')
         for i in range(len(rs)):
-            rs[i] = sorted(rs[i], key=lambda x:(x[1], -x[0]))
+            rs[i] = sorted(rs[i], key=lambda x: (x[1], -x[0]))
         fd.close()
         shutil.rmtree(self.data_path + "_1")
         self.assertEqual(rs, self.toy_exp)
@@ -49,11 +49,10 @@ class TestFIndexer(unittest.TestCase):
         fd.add(self.toy_label, self.toy_data.tobytes())
         rs = fd.query(self.toy_query.tobytes(), 2, method='force')
         for i in range(len(rs)):
-            rs[i] = sorted(rs[i], key=lambda x:(x[1], -x[0]))
+            rs[i] = sorted(rs[i], key=lambda x: (x[1], -x[0]))
         fd.close()
         shutil.rmtree(self.data_path + "_2")
         self.assertEqual(rs, self.toy_exp)
-
 
     def test_dump_load(self):
         fd = BIndexer(self.toy_data.shape[1], data_path=self.data_path + "_3")
@@ -65,7 +64,7 @@ class TestFIndexer(unittest.TestCase):
         fd2 = BIndexer.load(self.dump_path)
         rs = fd2.query(self.toy_query.tobytes(), 2)
         for i in range(len(rs)):
-            rs[i] = sorted(rs[i], key=lambda x:(x[1], -x[0]))
+            rs[i] = sorted(rs[i], key=lambda x: (x[1], -x[0]))
         fd2.close()
 
         shutil.rmtree(self.data_path + "_3")
