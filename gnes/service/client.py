9de5aa1892cb361f8a09c79dd5e24072e555d0c3
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-05-13 15:24:17 +0800

    bug fix

--- gnes/service/client.py
+++ gnes/service/client.py
@@ -115,7 +115,6 @@ class ClientService(BS):
 
         if self.args.wait_reply:
             self.is_handler_done.wait(self.args.timeout)
-            while not self.result():
-                time.sleep(1)
-            res = self.result.pop()
-            return res
+            if self.result:
+                res = self.result.pop()
+                return res
