a18093430779cff2169542b3136a20eff0132258
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-05-14 20:00:25 +0800

    c

--- gnes/indexer/faiss.py
+++ gnes/indexer/faiss.py
@@ -44,7 +44,7 @@ class FaissIndexer(BaseIndexer):
             self.logger.warning('fail to load model from %s, will init an empty one' % self.indexer_file_path)
             self._faiss_index = faiss.index_factory(self.num_dim, self.index_key)
 
-    def add(self, doc_ids: List[int], vectors: np.ndarray, *args, **kwargs):
+    def add(self, doc_ids: List, vectors: np.ndarray, *args, **kwargs):
         if len(vectors) != len(doc_ids):
             raise ValueError("vectors length should be equal to doc_ids")
 
@@ -58,18 +58,16 @@ class FaissIndexer(BaseIndexer):
             self._doc_ids = doc_ids
         self._faiss_index.add(vectors)
 
-    def query(self, keys: np.ndarray, top_k: int, *args, **kwargs) -> List[List[Tuple[int, float]]]:
+    def query(self, keys: np.ndarray, top_k: int, *args, **kwargs) -> List[List[Tuple]]:
         if keys.dtype != np.float32:
             raise ValueError("vectors should be ndarray of float32")
 
         score, ids = self._faiss_index.search(keys, top_k)
-        print(score, ids)
         ret = []
         for _id, _score in zip(ids, score):
             ret_i = []
             for _id_i, _score_i in zip(_id, _score):
-                print(_id_i, _score_i, self._doc_ids[_id_i])
-                ret_i.append((int(self._doc_ids[_id_i]), _score_i))
+                ret_i.append((self._doc_ids[_id_i], _score_i))
             ret.append(ret_i)
 
         return ret
