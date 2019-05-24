c7a52a6b15ea1bc69bb611c8ddd2468c7d8b57fd
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-05-24 14:53:26 +0800

    http service modify

--- gnes/service/http.py
+++ gnes/service/http.py
@@ -180,14 +180,10 @@ class HttpService:
                                        self.args.socket_out,
                                        None)
             send_message(out_sock, message, timeout=self.args.timeout)
-            N, res = 0, None
-            while N < 10:
-                res = recv_message(in_sock)
-                if res:
-                    break
-                else:
-                    time.sleep(0.2)
-                    N += 1
+            try:
+                res = recv_message(in_sock, timeout=2)
+            except:
+                res = None
             self.logger.info('message received, closing socket')
             in_sock.close()
             out_sock.close()
