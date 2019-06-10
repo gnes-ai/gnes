cdd03c09773ba0309d92da11388046389ba1931c
felix <madwang@tencent.com>
2019-06-10 14:41:38 +0800

    ...

--- tests/test_grpc.py
+++ tests/test_grpc.py
@@ -12,6 +12,7 @@ from gnes.service.proxy import ProxyService
 class TestService(unittest.TestCase):
 
     def test_grpc(self):
+        # https://github.com/grpc/grpc/issues/9987#issuecomment-370156099
         p_args = set_proxy_service_parser().parse_args([
             '--socket_in',
             'PULL_BIND',
@@ -21,7 +22,7 @@ class TestService(unittest.TestCase):
         g_args = set_grpc_frontend_parser().parse_args([])
         with ProxyService(p_args), GRPCFrontend(g_args):
             with grpc.insecure_channel(
-                    '%s:%s' % (g_args.grpc_host, g_args.grpc_port),
+                    '%s:%s' % ('127.0.0.1', g_args.grpc_port),
                     options=[('grpc.max_send_message_length', 50 * 1024 * 1024),
                              ('grpc.max_receive_message_length', 50 * 1024 * 1024)]) as channel:
                 stub = gnes_pb2_grpc.GnesRPCStub(channel)
