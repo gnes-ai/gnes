690e305631669f553093ce58b3bd4cdc0129462b
felix <madwang@tencent.com>
2019-06-20 11:12:28 +0800

    ...

--- .travis/download.sh
+++ .travis/download.sh
@@ -14,11 +14,11 @@ function donwload_models() {
         && echo "download pretrained openai_gpt model ..."
         && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt.tar.bz2 -qO openai_gpt.tar.bz2 \
         && tar -xvjf openai_gpt.tar.bz2; rm openai_gpt.tar.bz2 \
-        && echo "donwload pretrained openai_gpt2 model ..."
+        && echo "donwload pretrained openai_gpt2 model ..." \
         && https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt2.tar.bz2 -qO openai_gpt2.tar.bz2 \
         && tar -xvjf openai_gpt2.tar.bz2; rm openai_gpt2.tar.bz2 \
-        && echo "donwload pretrained transformer_xl_wt103 model ..."
-        && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/transformer_xl_wt103.tar.bz2 -qO transformer_xl_wt103.tar.bz2\
+        && echo "donwload pretrained transformer_xl_wt103 model ..." \
+        && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/transformer_xl_wt103.tar.bz2 -qO transformer_xl_wt103.tar.bz2 \
         && tar -xvjf transformer_xl_wt103.tar.bz2; rm transformer_xl_wt103.tar.bz2
 }
 
