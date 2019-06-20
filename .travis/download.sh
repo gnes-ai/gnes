0999404a0406484009d19f6f51fbf3d3af17c9e5
felix <madwang@tencent.com>
2019-06-20 11:34:09 +0800

    ...

--- .travis/download.sh
+++ .travis/download.sh
@@ -10,7 +10,7 @@ function donwload_models() {
         && echo "download pretrained bert-cn model ..." \
         && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/zhs.model.tar.bz2 -qO zhs.model.tar.bz2 \
         && tar -xvjf zhs.model.tar.bz2; rm zhs.model.tar.bz2 \
-        && echo "download pretrained openai_gpt model ..."
+        && echo "download pretrained openai_gpt model ..." \
         && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt.tar.bz2 -qO openai_gpt.tar.bz2 \
         && tar -xvjf openai_gpt.tar.bz2; rm openai_gpt.tar.bz2 \
         && echo "donwload pretrained openai_gpt2 model ..." \
@@ -18,8 +18,7 @@ function donwload_models() {
         && tar -xvjf openai_gpt2.tar.bz2; rm openai_gpt2.tar.bz2 \
         && echo "donwload pretrained transformer_xl_wt103 model ..." \
         && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/transformer_xl_wt103.tar.bz2 -qO transformer_xl_wt103.tar.bz2 \
-        && tar -xvjf transformer_xl_wt103.tar.bz2; rm transformer_xl_wt103.tar.bz2 \
-        && cd ${HOME}
+        && tar -xvjf transformer_xl_wt103.tar.bz2; rm transformer_xl_wt103.tar.bz2
 }
 
 if [ "${TRAVIS_OS_NAME}" == linux ]; then
