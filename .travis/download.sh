0c869f97d7274fb0f711d2166d86b49801af63c6
felix <madwang@tencent.com>
2019-06-20 11:43:46 +0800

    ...

--- .travis/download.sh
+++ .travis/download.sh
@@ -6,19 +6,31 @@ set -e  # fail and exit on any command erroring
 : "${MODEL_DIR:?}"
 
 function donwload_models() {
-    cd ${MODEL_DIR} \
-        && echo "download pretrained bert-cn model ..." \
-        && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/zhs.model.tar.bz2 -qO zhs.model.tar.bz2 \
-        && tar -xvjf zhs.model.tar.bz2; rm zhs.model.tar.bz2 \
-        && echo "download pretrained openai_gpt model ..." \
-        && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt.tar.bz2 -qO openai_gpt.tar.bz2 \
-        && tar -xvjf openai_gpt.tar.bz2; rm openai_gpt.tar.bz2 \
-        && echo "donwload pretrained openai_gpt2 model ..." \
-        && https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt2.tar.bz2 -qO openai_gpt2.tar.bz2 \
-        && tar -xvjf openai_gpt2.tar.bz2; rm openai_gpt2.tar.bz2 \
-        && echo "donwload pretrained transformer_xl_wt103 model ..." \
-        && wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/transformer_xl_wt103.tar.bz2 -qO transformer_xl_wt103.tar.bz2 \
-        && tar -xvjf transformer_xl_wt103.tar.bz2; rm transformer_xl_wt103.tar.bz2
+    cd ${MODEL_DIR}
+
+    if [ ! -f zhs.model.tar.bz2 ]; then
+        echo "download pretrained bert-cn model ..."
+        wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/zhs.model.tar.bz2 -qO zhs.model.tar.bz2
+        tar -xvjf zhs.model.tar.bz2
+    fi
+
+    if [ ! -f openai_gpt.tar.bz2 ]; then
+        echo "download pretrained openai_gpt model ..."
+        wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt.tar.bz2 -qO openai_gpt.tar.bz2
+        tar -xvjf openai_gpt.tar.bz2
+    fi
+
+    if [ ! -f openai_gpt2.tar.bz2 ]; then
+        echo "donwload pretrained openai_gpt2 model ..."
+        wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/openai_gpt2.tar.bz2 -qO openai_gpt2.tar.bz2
+        tar -xvjf openai_gpt2.tar.bz2
+    fi
+
+    if [ ! -f openai_gpt2.tar.bz2 ]; then
+        echo "donwload pretrained transformer_xl_wt103 model ..."
+        wget https://gnes-1252847528.cos.ap-guangzhou.myqcloud.com/transformer_xl_wt103.tar.bz2 -qO transformer_xl_wt103.tar.bz2
+        tar -xvjf transformer_xl_wt103.tar.bz2
+    fi
 }
 
 if [ "${TRAVIS_OS_NAME}" == linux ]; then
