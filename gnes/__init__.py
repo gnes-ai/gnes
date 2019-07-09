6acbbc8dae42e4fd0e16b9949d2d35640ac496c1
hanhxiao <hanhxiao@tencent.com>
2019-07-09 17:36:38 +0800

    bumping version to v0.0.11

--- gnes/__init__.py
+++ gnes/__init__.py
@@ -15,4 +15,4 @@
 
 # pylint: disable=low-comment-ratio
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
--- release.sh
+++ release.sh
@@ -51,7 +51,7 @@ fi
 
 
 #$(grep "$VER_TAG" $CLIENT_CODE | sed -n 's/^.*'\''\([^'\'']*\)'\''.*$/\1/p')
-VER=$(git tag -l |tail -n1)
+VER=$(git tag -l | sort -V |tail -n1)
 printf "current version:\t\e[1;33m$VER\e[0m\n"
 
 VER=$(echo $VER | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
