941935286ef7477c848a7b98ebbbcbb46a3e60f0
hanhxiao <hanhxiao@tencent.com>
2019-07-11 14:09:29 +0800

    ci(wechat-work): restyling wechat msg

--- shell/push-wechatwork.sh
+++ shell/push-wechatwork.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-TEMPLATE="{\"msgtype\": \"markdown\", \"markdown\": {\"content\": \"# $MSG_TITLE\n > $MSG_CONTENT\n\n > 🔗 [View] $MSG_LINK\"}}"
+TEMPLATE="{\"msgtype\": \"markdown\", \"markdown\": {\"content\": \"# $MSG_TITLE\n > $MSG_CONTENT\n\n > 🔗 [$MSG_LINK]($MSG_LINK)\"}}"
 
-echo $TEMPLATE | curl -s $BOT_URL \
-   -H 'Content-Type: application/json' \
-   --data-binary "@-"
+echo $TEMPLATE | curl -s $BOT_URL -H 'Content-Type: application/json' --data-binary "@-"
