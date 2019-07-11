07a0308607417afd9798063064336a9df4ddf2e2
hanhxiao <hanhxiao@tencent.com>
2019-07-11 13:32:37 +0800

    ci(wechat-work): fix wechat bot id

--- shell/push-wechatwork.sh
+++ shell/push-wechatwork.sh
@@ -4,6 +4,8 @@ set -ex
 
 TEMPLATE="{\"msgtype\": \"markdown\", \"markdown\": {\"content\": \"# $MSG_TITLE\n > $MSG_CONTENT\n\n > 🔗 [View] $MSG_LINK\"}}"
 
+echo $BOT_ID
+
 echo $TEMPLATE | curl -s 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='$BOT_ID \
    -H 'Content-Type: application/json' \
    --data-binary "@-"
