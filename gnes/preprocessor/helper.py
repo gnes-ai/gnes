f58049ec8b0097dcafceb3bdccd8d1fcac0ce56e
felix <madwang@tencent.com>
2019-07-11 22:27:05 +0800

    refactor ffmpeg preprocessor

--- gnes/preprocessor/helper.py
+++ gnes/preprocessor/helper.py
@@ -39,24 +39,27 @@ def get_video_frames(buffer_data, image_format="cv2", **kwargs):
     # (-) output to stdout pipeline
     ffmpeg_cmd += ['-c:v', 'png', '-an', '-sn', '-']
 
-    pipe = sp.Popen(ffmpeg_cmd, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=-1, shell=False)
+    with sp.Popen(ffmpeg_cmd, stdin=sp.PIPE, stdout=sp.PIPE, bufsize=-1, shell=False) as pipe:
+        stream, _ = pipe.communicate(buffer_data)
 
-    stream, _ = pipe.communicate(buffer_data)
+        # raw bytes for multiple PNGs.
+        # split by PNG EOF b'\x89PNG'
+        stream = stream.split(b'\x89PNG')
 
-    # raw bytes for multiple PNGs.
-    # split by PNG EOF b'\x89PNG'
-    stream = stream.split(b'\x89PNG')
+        if len(stream) <= 1:
+            return []
 
-    # reformulate the full pngs for feature processings.
-    if image_format == 'pil':
-        frames = [Image.open(io.BytesIO(b'\x89PNG' + _)) for _ in stream[1:]]
-    elif image_format == 'cv2':
-        frames = [
-            cv2.imdecode(np.frombuffer(b'\x89PNG' + _, np.uint8), 1)
-            for _ in stream[1:]
-        ]
-    else:
-        raise NotImplementedError
+        # reformulate the full pngs for feature processings.
+        if image_format == 'pil':
+            frames = [Image.open(io.BytesIO(b'\x89PNG' + _)) for _ in stream[1:]]
+        elif image_format == 'cv2':
+            frames = [
+                cv2.imdecode(np.frombuffer(b'\x89PNG' + _, np.uint8), 1)
+                for _ in stream[1:]
+            ]
+        else:
+            raise NotImplementedError
+
     return frames
 
 
@@ -136,3 +139,54 @@ def compare_descriptor(descriptor1, descriptor2, metric="chisqr"):
     }
 
     return cv2.compareHist(descriptor1, descriptor2, dist_metric[metric])
+
+def pil_to_array(pil_image: Image):
+    """
+    Load a PIL image and return it as a numpy array.  For grayscale
+    images, the return array is MxN.  For RGB images, the return value
+    is MxNx3.  For RGBA images the return value is MxNx4
+    """
+
+    def toarray(im, dtype=np.uint8):
+        """Return a 1D array of dtype."""
+        # Pillow wants us to use "tobytes"
+        if hasattr(im, 'tobytes'):
+            x_str = im.tobytes('raw', im.mode)
+        else:
+            x_str = im.tostring('raw', im.mode)
+        x = np.fromstring(x_str, dtype)
+        return x
+
+    if pil_image.mode in ('RGBA', 'RGBX'):
+        im = pil_image    # no need to convert images
+    elif pil_image.mode == 'L':
+        im = pil_image    # no need to luminance images
+        # return MxN luminance array
+        x = toarray(im)
+        x.shape = im.size[1], im.size[0]
+        return x
+    elif pil_image.mode == 'RGB':
+        # return MxNx3 RGB array
+        im = pil_image    # no need to RGB images
+        x = toarray(im)
+        x.shape = im.size[1], im.size[0], 3
+        return x
+    elif pil_image.mode.startswith('I;16'):
+        # return MxN luminance array of uint16
+        im = pil_image
+        if im.mode.endswith('B'):
+            x = toarray(im, '>u2')
+        else:
+            x = toarray(im, '<u2')
+        x.shape = im.size[1], im.size[0]
+        return x.astype('=u2')
+    else:    # try to convert to an rgba image
+        try:
+            im = pil_image.convert('RGBA')
+        except ValueError:
+            raise RuntimeError('Unknown image mode')
+
+    # return MxNx4 RGBA array
+    x = toarray(im)
+    x.shape = im.size[1], im.size[0], 4
+    return x
