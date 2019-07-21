2d5654c0466408251ede54a1eadf25a4e2e567bc
hanhxiao <hanhxiao@tencent.com>
2019-07-21 08:39:37 +0800

    chore(license): add license

--- gnes/composer/flask.py
+++ gnes/composer/flask.py
@@ -1,3 +1,18 @@
+#  Tencent is pleased to support the open source community by making GNES available.
+#
+#  Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import tempfile
 
 from .base import YamlComposer
@@ -8,7 +23,7 @@ class YamlComposerFlask:
     def __init__(self, args):
         self.args = args
 
-    def create_flask_app(self):
+    def _create_flask_app(self):
         try:
             from flask import Flask, request, abort, redirect, url_for
             from flask_compress import Compress
@@ -22,11 +37,11 @@ class YamlComposerFlask:
         app = Flask(__name__)
 
         @app.route('/', methods=['GET'])
-        def get_homepage():
+        def _get_homepage():
             return YamlComposer(set_composer_parser().parse_args([])).build_all()['html']
 
         @app.route('/refresh', methods=['POST'])
-        def regenerate():
+        def _regenerate():
             data = request.form if request.form else request.json
             f = tempfile.NamedTemporaryFile('w', delete=False).name
             with open(f, 'w', encoding='utf8') as fp:
@@ -43,5 +58,5 @@ class YamlComposerFlask:
         return app
 
     def run(self):
-        app = self.create_flask_app()
+        app = self._create_flask_app()
         app.run(port=self.args.http_port, threaded=True, host='0.0.0.0')
