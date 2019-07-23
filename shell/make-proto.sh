1011fb7fcce4f05a30ca8342858a870709f92c5d
Felix <35718120+numb3r3@users.noreply.github.com>
2019-07-23 12:09:35 +0800

    Update make-proto.sh
    fix(proto): use the default grpc_python_plugin path

--- shell/make-proto.sh
+++ shell/make-proto.sh
@@ -2,7 +2,7 @@
 
 SRC_DIR=../gnes/proto/
 #PLUGIN_PATH=/Volumes/TOSHIBA-4T/Documents/grpc/bins/opt/grpc_python_plugin
-PLUGIN_PATH=${HOME}/Desktop/git/grpc/bins/opt/grpc_python_plugin
+PLUGIN_PATH=/user/local/grpc/bins/opt/grpc_python_plugin
 
 protoc -I ${SRC_DIR} --python_out=${SRC_DIR} --grpc_python_out=${SRC_DIR} --plugin=protoc-gen-grpc_python=${PLUGIN_PATH} ${SRC_DIR}gnes.proto
 
