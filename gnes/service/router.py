9c33dc66a2d1eb478138851ebb0fa27ebfcad9c5
hanhxiao <hanhxiao@tencent.com>
2019-07-29 14:02:27 +0800

    feat(router): allow consecutive mapping and reducing ops

--- gnes/service/router.py
+++ gnes/service/router.py
@@ -32,7 +32,7 @@ class RouterService(BS):
         self._pending = defaultdict(list)  # type: Dict[str, List]
 
     def _is_msg_complete(self, msg: 'gnes_pb2.Message', num_req: int) -> bool:
-        return (hasattr(self.args, 'num_part') and num_req == self.args.num_part) or (num_req == msg.envelope.num_part)
+        return (hasattr(self.args, 'num_part') and num_req == self.args.num_part[-1]) or (num_req == msg.envelope.num_part[-1])
 
     @handler.register(NotImplementedError)
     def _handler_default(self, msg: 'gnes_pb2.Message'):
