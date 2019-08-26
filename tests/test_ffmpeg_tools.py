0739dc5a2f6899fff412bc97b4ef5d8c6a6c5e53
felix <madwang@tencent.com>
2019-08-26 18:51:56 +0800

    ...

--- tests/test_ffmpeg_tools.py
+++ tests/test_ffmpeg_tools.py
@@ -15,14 +15,12 @@ class TestFFmpeg(unittest.TestCase):
         self.video_path = os.path.join(self.dirname, 'videos', 'test.mp4')
         self.frames = video.capture_frames(input_fn=self.video_path, fps=10, scale='768:360')
 
-    @unittest.SkipTest
     def test_probe(self):
         probe = ffmpeg.probe(self.video_path)
         self.assertEqual(probe['height'], 720)
         self.assertEqual(probe['width'], 1280)
         self.assertEqual(probe['fps'], 25.0)
 
-    @unittest.SkipTest
     def test_capture_frames(self):
         frames1 = video.capture_frames(input_fn=self.video_path, fps=10, scale='768:360')
 
@@ -32,27 +30,23 @@ class TestFFmpeg(unittest.TestCase):
 
         self.assertEqual(frames1.shape, frames2.shape)
 
-    @unittest.SkipTest
     def test_scale_video(self):
         out = video.scale_video(input_fn=self.video_path, scale='768:360')
         meta = ffmpeg.get_media_meta(input_data=out, input_options={'format': 'mp4'})
         self.assertEqual(meta['frame_width'], 768)
         self.assertEqual(meta['frame_height'], 360)
 
-    @unittest.SkipTest
     def test_encode_video(self):
         video_data = video.encode_video(images=list(self.frames))
         meta = ffmpeg.get_media_meta(input_data=video_data, input_options={'format': 'mp4'})
         self.assertEqual(meta['frame_width'], 768)
         self.assertEqual(meta['frame_height'], 360)
 
-    @unittest.SkipTest
     def test_gif_encode(self):
         gif_data = gif.encode_gif(images=list(self.frames), fps=10)
         frames = gif.decode_gif(data=gif_data)
         self.assertEqual(self.frames.shape, frames.shape)
 
-    @unittest.SkipTest
     def test_capture_audio(self):
         audio_data1 = audio.capture_audio(input_fn=self.video_path)
         with open(self.video_path, 'rb') as f:
