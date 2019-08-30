64cea23ee038eaf46a9de6ef326daef0e1fad6e4
Jem <jif0729@gmail.com>
2019-08-30 18:10:48 +0800

    feat(chunk scorer): add offset divergence

--- gnes/indexer/base.py
+++ gnes/indexer/base.py
@@ -152,6 +152,45 @@ class DocScorer:
         })
         return s
 
+    @staticmethod
+    def eq2(q_chunk: 'gnes_pb2.Chunk', d_chunk: 'gnes_pb2.Chunk',
+            relevance):
+        """
+        score = d_chunk.weight * relevance * offset_divergence * q_chunk.weight
+        offset_divergence is calculated based on doc_type:
+            TEXT && VIDEO && AUDIO: offset is 1-D
+            IMAGE: offset is 2-D
+        """
+
+        def _cal_divergence(q_chunk: 'gnes_pb2.Chunk', d_chunk: 'gnes_pb2.Chunk'):
+            if q_chunk.offset_nd and d_chunk.offset_nd:
+                return 1 / (1 + np.sqrt((q_chunk.offset_nd[0] - d_chunk.offset_nd[0]) ** 2 +
+                                        (q_chunk.offset_nd[1] - d_chunk.offset_nd[1]) ** 2))
+            else:
+                return np.abs(q_chunk.offset - d_chunk.offset)
+
+        score = gnes_pb2.Response.QueryResponse.ScoredResult.Score()
+
+        divergence = _cal_divergence(q_chunk, d_chunk)
+        score.value = d_chunk.weight * relevance * divergence * q_chunk.weight
+        score.explained = json.dumps({
+            'name': 'chunk-eq2',
+            'operand': [{'name': 'd_chunk_weight',
+                         'value': d_chunk.weight,
+                         'doc_id': d_chunk.doc_id,
+                         'offset': d_chunk.offset},
+                        {'name': 'q_chunk_weight',
+                         'value': q_chunk.weight,
+                         'offset': q_chunk.offset},
+                        {'name': 'relevance',
+                         'value': relevance},
+                        {'name': 'offset_divergence',
+                         'value': divergence}],
+            'op': 'prod',
+            'value': score.value
+        })
+        return score
+
 
 class JointIndexer(CompositionalTrainableBase):
 
